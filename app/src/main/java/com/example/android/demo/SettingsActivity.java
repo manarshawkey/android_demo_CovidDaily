5b8fafd617a01b918add297b6fb07cb19a9b2dfa
manarshawkey <manarshawkey97@gmail.com>
2022-03-22 16:30:47 +0200

    adding shared preferences

--- app/src/main/java/com/example/android/demo/SettingsActivity.java
+++ app/src/main/java/com/example/android/demo/SettingsActivity.java
@@ -1,8 +1,13 @@
 package com.example.android.demo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings_container, new SettingsFragment())
+                .commit();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            Intent backHome = new Intent(this, MainActivity.class);
+            startActivity(backHome);
+        }
+        return true;
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat{
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preferences, rootKey);
+        }
     }
 }
