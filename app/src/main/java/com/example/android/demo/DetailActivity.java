f906da332bf21de70e26b6803c74c09df1eca91e
manarshawkey <manarshawkey97@gmail.com>
2022-03-26 21:48:08 +0200

    populating views with data

--- app/src/main/java/com/example/android/demo/DetailActivity.java
+++ app/src/main/java/com/example/android/demo/DetailActivity.java
@@ -6,10 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
 
     private final String LOG_TAG = DetailActivity.class.getSimpleName();
+    private TextView mDate;
+    private TextView mNewConfirmed;
+    private TextView mTotalConfirmed;
+    private TextView mNewDeaths;
+    private TextView mTotalDeaths;
+    private TextView mTotalActive;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,18 @@ public class DetailActivity extends AppCompatActivity {
         Bundle bundle = intent.getExtras();
         if(bundle != null && bundle.containsKey("Record")) {
             CovidRecord currentRecord = (CovidRecord) bundle.getSerializable("Record");
+            mDate = findViewById(R.id.textView_date);
+            mDate.setText(String.valueOf(currentRecord.getDate()));
+            mNewConfirmed = findViewById(R.id.textView_newConfirmedCases);
+            mNewConfirmed.setText(String.valueOf(currentRecord.getConfirmedCases()));
+            mTotalConfirmed = findViewById(R.id.textView_totalConfirmedCases);
+            mTotalConfirmed.setText(String.valueOf(currentRecord.getTotalConfirmed()));
+            mNewDeaths = findViewById(R.id.textView_newDeaths);
+            mNewDeaths.setText(String.valueOf(currentRecord.getDeaths()));
+            mTotalDeaths = findViewById(R.id.textView_totalDeaths);
+            mTotalDeaths.setText(String.valueOf(currentRecord.getTotalDeaths()));
+            mTotalActive = findViewById(R.id.textView_totalActiveCases);
+            mTotalActive.setText(String.valueOf(currentRecord.getActive()));
             Log.d(LOG_TAG, currentRecord.getDate());
         }
     }
