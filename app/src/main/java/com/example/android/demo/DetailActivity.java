43069a7300bf883b2daccc0c08ef47403ce37d1b
manarshawkey <manarshawkey97@gmail.com>
2022-03-26 22:28:41 +0200

    refactoring

--- app/src/main/java/com/example/android/demo/DetailActivity.java
+++ app/src/main/java/com/example/android/demo/DetailActivity.java
@@ -6,12 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,36 +41,39 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-
     private void setupFAB() {
-        mShareFAB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.setType("text/plain");
-                StringBuilder text = new StringBuilder();
-                text.append("Checkout Covid Daily Report\n\n");
-                text.append(getResources().getString(R.string.label_date));
-                text.append(": ").append(mCurrentRecord.getDate()).append('\n');
-                text.append(getResources().getString(R.string.label_newConfirmedCases));
-                text.append(": ").append(mCurrentRecord.getConfirmedCases()).append('\n');
-                text.append(getResources().getString(R.string.label_totalConfirmedCases));
-                text.append(": ").append(mCurrentRecord.getTotalConfirmed()).append('\n');
-                text.append(getResources().getString(R.string.label_totalActiveCases));
-                text.append(": ").append(mCurrentRecord.getActive()).append('\n');
-                text.append(getResources().getString(R.string.label_newDeaths));
-                text.append(": ").append(mCurrentRecord.getDeaths()).append('\n');
-                text.append(getResources().getString(R.string.label_totalDeaths));
-                text.append(": ").append(mCurrentRecord.getTotalDeaths()).append('\n');
-                sendIntent.putExtra(Intent.EXTRA_TEXT, text.toString());
+        mShareFAB.setOnClickListener(view -> {
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.setType("text/plain");
+            String text = formatSharedData();
 
-                Intent shareIntent = Intent.createChooser(sendIntent,
-                        getString(R.string.title_shareIntent));
-                startActivity(shareIntent);
-            }
+            sendIntent.putExtra(Intent.EXTRA_TEXT, text);
+
+            Intent shareIntent = Intent.createChooser(sendIntent,
+                    getString(R.string.title_shareIntent));
+            startActivity(shareIntent);
         });
     }
+
+    private String formatSharedData() {
+        StringBuilder text = new StringBuilder();
+        text.append("Checkout Covid Daily Report\n\n");
+        text.append(getResources().getString(R.string.label_date));
+        text.append(": ").append(mCurrentRecord.getDate()).append('\n');
+        text.append(getResources().getString(R.string.label_newConfirmedCases));
+        text.append(": ").append(mCurrentRecord.getConfirmedCases()).append('\n');
+        text.append(getResources().getString(R.string.label_totalConfirmedCases));
+        text.append(": ").append(mCurrentRecord.getTotalConfirmed()).append('\n');
+        text.append(getResources().getString(R.string.label_totalActiveCases));
+        text.append(": ").append(mCurrentRecord.getActive()).append('\n');
+        text.append(getResources().getString(R.string.label_newDeaths));
+        text.append(": ").append(mCurrentRecord.getDeaths()).append('\n');
+        text.append(getResources().getString(R.string.label_totalDeaths));
+        text.append(": ").append(mCurrentRecord.getTotalDeaths()).append('\n');
+        return text.toString();
+    }
+
     private void displayData() {
         if(mCurrentRecord != null) {
             mDate.setText(String.valueOf(mCurrentRecord.getDate()));
